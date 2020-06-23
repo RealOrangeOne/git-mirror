ba2cc72b6d5764cebf4ac7a4101f6cc1bf652418
Jake Howard <git@theorangeone.net>
2020-06-23 20:53:52 +0100

    Create a config class to deal with all the parsing

--- git_mirror/cli.py
+++ git_mirror/cli.py
@@ -4,8 +4,10 @@ import os
 import shutil
 import subprocess
 from pathlib import Path
+from typing import List
 
 import toml
+from pydantic import Field
 from pydantic.dataclasses import dataclass
 
 
@@ -16,7 +18,9 @@ class Repo:
 
     @property
     def directory_name(self) -> str:
-        return hashlib.sha1((self.source + self.destination).encode()).hexdigest()
+        return hashlib.sha1(
+            (self.expanded_source + self.expanded_destination).encode()
+        ).hexdigest()
 
     def display(self) -> str:
         return f"{self.source} -> {self.destination}"
@@ -30,53 +34,67 @@ class Repo:
         return os.path.expandvars(self.destination)
 
 
-def get_repos():
-    config_file = Path() / "repos.toml"
-    config = toml.loads(config_file.read_text())
+@dataclass
+class Config:
+    repo: List[Repo] = Field(default_factory=list)
+    clone_root: Path = Path(__file__).resolve().parent / "repos"
 
-    repos = []
-    for repo_config in config.get("repo", []):
-        repos.append(Repo(**repo_config))
-    return repos
+    @classmethod
+    def from_file(cls, file: Path) -> "Config":
+        return cls(**toml.loads(file.read_text()))
+
+    @property
+    def repos(self) -> List[Repo]:
+        """
+        Stupid TOML
+        """
+        return self.repo
+
+    def directory_for_repo(self, repo: Repo) -> Path:
+        return self.clone_root / repo.directory_name
 
 
-def git(*command: str, cwd=None) -> str:
-    git_command = ["git", *command]
-    return subprocess.check_output(git_command, cwd=cwd or Path(),).decode()
+def git(command: List[str], cwd: Path) -> str:
+    return subprocess.check_output(["git"] + command, cwd=cwd).decode()
 
 
-def get_repo(repo: Repo, directory: Path):
+def get_repo(config: Config, repo: Repo):
+    directory = config.directory_for_repo(repo)
     if directory.exists():
-        git("fetch", "--quiet", cwd=directory)
+        git(["fetch", "--quiet"], cwd=directory)
     else:
-        git("clone", "--quiet", "--bare", repo.expanded_source, str(directory))
+        git(
+            ["clone", "--quiet", "--bare", repo.expanded_source, str(directory)],
+            cwd=config.clone_root,
+        )
 
 
-def push_repo(repo: Repo, directory: Path):
+def push_repo(config: Config, repo: Repo):
+    directory = config.directory_for_repo(repo)
     assert directory.exists()
-    git("push", "--mirror", "--quiet", repo.expanded_destination, cwd=directory)
+    git(["push", "--mirror", "--quiet", repo.expanded_destination], cwd=directory)
 
 
 def git_gc(directory: Path):
-    git("gc", "--auto", "--quiet", cwd=directory)
+    git(["gc", "--auto", "--quiet"], cwd=directory)
 
 
 def main():
-    repos_dir = Path().resolve() / "repos"
-    repos = get_repos()
-    repos_dir.mkdir(exist_ok=True)
-    for repo in repos:
+    config = Config.from_file(Path() / "repos.toml")
+    config.clone_root.mkdir(exist_ok=True)
+    for repo in config.repos:
         print("Updating", repo.display())
-        repo_dir = repos_dir / repo.directory_name
-        get_repo(repo, repo_dir)
-        push_repo(repo, repo_dir)
+        repo_dir = config.clone_root / repo.directory_name
+        get_repo(config, repo)
+        push_repo(config, repo)
         git_gc(repo_dir)
-    active_repo_dirs = {repo.directory_name for repo in repos}
-    all_repos = {d.name for d in repos_dir.iterdir() if d.is_dir()}
+
+    active_repo_dirs = {repo.directory_name for repo in config.repos}
+    all_repos = {d.name for d in config.clone_root.iterdir() if d.is_dir()}
     defunct_repos = all_repos - active_repo_dirs
     for defunct_repo in defunct_repos:
         print("Cleaning up", defunct_repo)
-        shutil.rmtree(repos_dir / defunct_repo)
+        shutil.rmtree(config.clone_root / defunct_repo)
 
 
 if __name__ == "__main__":
